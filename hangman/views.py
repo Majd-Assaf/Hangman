6da98e6f294b6eae003fc595f84a32f48476ef6e
Majd-Assaf <majdassaf81@gmail.com>
2022-06-08 09:11:31 +0200

    add Model and db

--- hangman/views.py
+++ hangman/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+
 # Create your views here.
 
 letters = ['a','b','c','d','e','f','g','h','i','j','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-word = ['d','e','u','t','s','c','h','l','a','n','d']
+word = ['M','e','u','t','s','c','h','l','a','n','d']
+
 
 def index(request):
     return render(request,"hangman/index.html",{'letters':letters , 'word':word})
